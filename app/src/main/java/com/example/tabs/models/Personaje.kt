46d98b8d5449e215702f414b6c32ecfc27e86384
cristianranged <crangeld@ufpso.edu.co>
2024-11-12 19:19:47 -0500

    Primer commit

---
+++ app/src/main/java/com/example/tabs/models/Personaje.kt
@@ -0,0 +1,12 @@
+package com.example.tabs.models
+
+data class Personaje(
+    val id: Number = 0,
+    val nombre: String,
+    val descripcion: String,
+    val principal: Int,
+    val secundario: Int,
+    val extra: Int,
+    val imagen: String,
+
+)
